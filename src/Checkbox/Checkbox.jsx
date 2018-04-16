e8803d4964c0786b93bc33913f9e5c1ee0f7e9e8
caizongding <caizongding@gmail.com>
2018-04-16 10:19:21 +0800

    编译es6和jsx文件

---
+++ src/Checkbox/Checkbox.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Checkbox extends Component {
+  render() {
+    return (
+      <div>
+        Checkbox
+      </div>
+    );
+  }
+}
+
+export default Checkbox;
